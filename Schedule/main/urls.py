4eb3535e9047f12bf8dee379f1c19ec0dc35297e
Vici <ripsodi@gmail.com>
2019-05-08 13:03:00 +0300

    Add pagination

--- Schedule/main/urls.py
+++ Schedule/main/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import (cian_render, delete_apartment,
-                    load_apartment, search_apartment)
+                    load_apartment, 
+                    # search_apartment
+                    )
 
 
 app_name = 'main'
@@ -9,5 +11,5 @@ urlpatterns = [
     path('', cian_render, name='cian_render'),
     path('delete', delete_apartment, name='delete_apartment'),
     path('load', load_apartment, name='load_apartment'),
-    path('search', search_apartment, name='search_apartment'),
+    # path('search', search_apartment, name='search_apartment'),
 ]
--- Schedule/main/views.py
+++ Schedule/main/views.py
@@ -1,13 +1,42 @@
 from django.shortcuts import render, redirect
 from django.contrib.postgres.search import SearchVector
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from .models import Apartment
 from .tasks import main
 
 
 def cian_render(request, queryset=None):
-    queryset = queryset or Apartment.objects.all()
-    context = {'ads': queryset}
+    query = request.GET.get('query')
+    if query:
+        search_vector = SearchVector('address', 'total_area', 'floor', 'price')
+        queryset = Apartment.objects.annotate(search=search_vector) \
+                                    .filter(search=request.GET.get('query'))
+    else:
+        queryset = queryset or Apartment.objects.all()
+
+    paginator = Paginator(queryset, 15)
+    page = request.GET.get('page')
+    
+    try:
+        items = paginator.page(page)
+    except PageNotAnInteger:
+        items = paginator.page(1)
+    except EmptyPage:
+        items = paginator.page(paginator.num_pages)
+
+    index = items.number - 1
+    max_index = len(paginator.page_range)
+    start_index = index - 5 if index >=5 else 0
+    end_index = index + 5 if index <= index - 5 else max_index
+    page_range = paginator.page_range[start_index:end_index]
+    
+    context = {
+        'items': items,
+        'page_range': page_range,
+        'ads_count': queryset.count(),
+        'query': query,
+    }
     return render(request, 'cian_table.html', context)
 
 def delete_apartment(request):
@@ -19,8 +48,8 @@ def load_apartment(request):
     main.delay()
     return redirect('main:cian_render')
 
-def search_apartment(request):
-    search_vector = SearchVector('address', 'total_area', 'floor', 'price')
-    queryset = Apartment.objects.annotate(search=search_vector) \
-                                .filter(search=request.GET.get('query'))
-    return cian_render(request, queryset)
+# def search_apartment(request):
+#     search_vector = SearchVector('address', 'total_area', 'floor', 'price')
+#     queryset = Apartment.objects.annotate(search=search_vector) \
+#                                 .filter(search=request.GET.get('query'))
+#     return cian_render(request, queryset)
